e35690fa3730102ec15aaaa01d4e4bc1508b3688
hp <bhagatnicky1234@gmail.com>
2024-09-04 14:31:42 +0530

    commit2

--- app/src/main/java/com/example/stepup_carbonid/Registration_page.java
+++ app/src/main/java/com/example/stepup_carbonid/Registration_page.java
@@ -1,6 +1,9 @@
 package com.example.stepup_carbonid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,7 @@ public class Registration_page extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
--- app/src/main/java/com/example/stepup_carbonid/login_page.java
+++ app/src/main/java/com/example/stepup_carbonid/login_page.java
@@ -1,6 +1,9 @@
 package com.example.stepup_carbonid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login_page extends AppCompatActivity {
-
+TextView reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class login_page extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        reg=findViewById(R.id.reg);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(login_page.this,Registration_page.class);
+                startActivity(i);
+            }
         });
     }
 }
